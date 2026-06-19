101a2cf2f7a831425b25740d7222418c8f133353
Sks <124144700+sks123git@users.noreply.github.com>
2026-06-19 15:32:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,8 +3,23 @@ getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
 
 const getSum = () => {
-//Add your code here
-  
+	const prices = document.querySelectorAll(".price");
+
+let total = 0;
+
+prices.forEach(price => {
+    total += Number(price.textContent);
+});
+
+const row = document.createElement("tr");
+
+const cell = document.createElement("td");
+cell.id = "ans";
+cell.colSpan = 2;
+cell.textContent = total;
+
+row.appendChild(cell);
+	document.querySelector("table").appendChild(row);
 };
 
 getSumBtn.addEventListener("click", getSum);
